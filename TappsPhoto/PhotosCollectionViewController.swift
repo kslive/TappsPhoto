cd6d297ae7c75ff1dad128648892f586ccda4940
ksllive <zhenya-kiselev-97@mail.ru>
2020-07-25 00:09:48 +0300

    Передача изображений в ячейки(UIImageView).

--- TappsPhoto/PhotosCollectionViewController.swift
+++ TappsPhoto/PhotosCollectionViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
     
+    let photos = ["photo2",
+                  "photo3",
+                  "photo4",
+                  "photo5",
+                  "photo6",
+                  "photo7",
+                  "photo8",
+                  "photo9",
+                  "photo10",
+                  "photo11",
+                  "photo12",
+                  "photo13",
+                  "photo14",
+                  "photo15"]
+    
     let itemsPerRow: CGFloat = 2
     let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
     }
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -23,13 +38,16 @@ class PhotosCollectionViewController: UICollectionViewController {
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1000
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
-        cell.backgroundColor = .black
+        
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
+        cell.randomPhotoImageView.image = image
         
         return cell
     }
@@ -39,7 +57,6 @@ class PhotosCollectionViewController: UICollectionViewController {
 extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
         
         let paddingWidth = sectionInserts.left * (itemsPerRow + 1)
         let availableWidth = collectionView.frame.width - paddingWidth
