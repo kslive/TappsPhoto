a16e6781bf60e84b56312f177e3295dd848cf274
ksllive <zhenya-kiselev-97@mail.ru>
2020-07-24 17:14:31 +0300

    set custom cell.

---
+++ TappsPhoto/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  TappsPhoto
+//
+//  Created by Eugene Kiselev on 24.07.2020.
+//  Copyright © 2020 Eugene Kiselev. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
